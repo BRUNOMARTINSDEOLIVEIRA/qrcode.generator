4ad2e95975d5b2ba46434b05bb6bf213cdee52e2
Bruno Martins <brunomartinsoliveira28@gmail.com>
2025-08-17 14:21:14 -0300

    Criando o Service de geracao do QR Code

---
+++ src/main/java/com/brunomartinsdeoliveira/qrcode/generator/service/QrCodeGeneratorService.java
@@ -0,0 +1,38 @@
+package com.brunomartinsdeoliveira.qrcode.generator.service;
+
+import com.brunomartinsdeoliveira.qrcode.generator.ports.StoragePort;
+import com.brunomartinsdeoliveira.qrcode.generator.dto.QrCodeGeneratorResponse;
+import com.google.zxing.BarcodeFormat;
+import com.google.zxing.WriterException;
+import com.google.zxing.client.j2se.MatrixToImageWriter;
+import com.google.zxing.common.BitMatrix;
+import com.google.zxing.qrcode.QRCodeWriter;
+import org.springframework.stereotype.Service;
+
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.util.UUID;
+
+@Service
+public class QrCodeGeneratorService {
+
+    private final StoragePort storage;
+
+    public QrCodeGeneratorService(StoragePort storage) {
+        this.storage = storage;
+    }
+
+    public QrCodeGeneratorResponse generateAndUploadQrCode(String text) throws WriterException, IOException {
+        QRCodeWriter qrCodeWriter = new QRCodeWriter();
+        BitMatrix bitMatrix = qrCodeWriter.encode(text, BarcodeFormat.QR_CODE, 200, 200);
+
+        ByteArrayOutputStream pngOutputStream = new ByteArrayOutputStream();
+        MatrixToImageWriter.writeToStream(bitMatrix, "PNG", pngOutputStream);
+        byte[] pngQrCodeData = pngOutputStream.toByteArray();
+
+        String fileName = UUID.randomUUID().toString();
+        String url = storage.uploadFile(pngQrCodeData, fileName, "image/png");
+
+        return new QrCodeGeneratorResponse(url);
+    }
+}
